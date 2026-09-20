d15b3530c46e9c0a61f8ae25dfc4701fc382cb7c
overnight-agent <overnight-agent@luminar.local>
2026-09-20 22:01:48 +0000

    KAN-5: Add active todo counter [slice:update_frontend_to_display_active_todo_count]
    Agent-Slice: kan_5:slice_2_update_frontend_to_display_active_todo_count
    Agent-Slice-Depends-On: kan_5:slice_1_implement_logic_to_count_active_todos
    Agent-Previous-Commit: e13067c

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 const TodoList = ({ todos, onToggleTodo, onDeleteTodo }) => {
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          <span 
-            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-            onClick={() => onToggleTodo(todo.id)}
-          >
-            {todo.text}
-          </span>
-          <button onClick={() => onDeleteTodo(todo.id)}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span 
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              onClick={() => onToggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => onDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <p>Active Todos: {todos.filter(todo => !todo.completed).length}</p>
+    </div>
   );
 };
 
